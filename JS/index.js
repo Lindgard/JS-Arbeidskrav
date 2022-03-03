b04ee423d005fb9786d5ab173c389c09f8524a93
Lindgard <a.b.lindgard@gmail.com>
2022-03-03 12:29:21 +0000

    completed price and item addition-function in purchases

--- JS/index.js
+++ JS/index.js
@@ -6,21 +6,48 @@ greeting();
 // Items not needed, in Storage
 let inputItem = document.getElementById("add-item-storage");
 let storageItemsList = document.getElementById("stored-items");
+let itemArrayStorage = [];
+
 function addItemStorage() {
   let newItemStorage = inputItem.value;
   storageItemsList.innerHTML += `<li>${newItemStorage}</li>`;
+  itemArrayStorage.push(newItemStorage);
 }
+
 // Items to purchase
 let inputPurchase = document.getElementById("add-item-purchase");
+let inputPrice = document.getElementById("price-add");
 let purchaseItemsList = document.getElementById("purchases");
+let itemArrayPurchase = [];
+
 function addItemPurchase() {
   let newItemPurchase = inputPurchase.value;
-  purchaseItemsList.innerHTML += `<li>${newItemPurchase}</li>`;
+  let newItemPrice = parseInt(inputPrice.value);
+  // console.log(newItemPrice);
+   if (newItemPrice === 0) {
+    alert("ERROR! Price cannot be 0!")
+    return
+  } else if (isNaN(newItemPrice)) {
+    alert("ERROR! Input has to be a number!")
+    return
+  }
+  purchaseItemsList.innerHTML += `<li>${newItemPurchase}, ${newItemPrice} kr</li>`;
+  itemArrayPurchase.push({
+    name: newItemPurchase,
+    price: newItemPrice,
+  });
+}
+
+function deleteItemPurchase () {
+  itemArrayPurchase.splice(0,1) = prompt ("vil du slette dette? skriv Ja");
 }
 // Low in stock
 let inputLowStorage = document.getElementById("add-item-low");
 let lowStorageItemsList = document.getElementById("low-storage");
+let itemArrayLow = [];
+
 function addItemLowStock() {
   let newItemLow = inputLowStorage.value;
   lowStorageItemsList.innerHTML += `<li>${newItemLow}</li>`;
+  itemArrayLow.push(newItemLow);
 }
