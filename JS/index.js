654d3b2850b9af350c93a069c81eb7ba25650459
Lindgard <a.b.lindgard@gmail.com>
2022-03-03 03:44:13 +0000

    functions adding items

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,19 @@
 function greeting() {
-    alert("Hei og velkommen til din handleliste!")
+  alert("Hei og velkommen til din handleliste!");
 }
 greeting();
 
+// Items not needed, in Storage
+let inputItem = document.getElementById("add-item-storage");
 let storageItemsList = document.getElementById("stored-items");
-function
-
+function addItemStorage() {
+  let newItem = inputItem.value;
+  storageItemsList.innerHTML += `<li>${newItem}</li>`;
+}
+// Items to purchase
+let inputPurchase = document.getElementById("add-item-purchase");
 let purchaseItemsList = document.getElementById("purchases");
 
+// Low in stock
+let inputLowStorage = document.getElementById("add-item-low");
 let lowStorageItemsList = document.getElementById("low-storage");
