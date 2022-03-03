8b1e94df0ccb32273cd5bc29df47687addb6d1c8
Lindgard <a.b.lindgard@gmail.com>
2022-03-03 13:03:29 +0000

    preparing delete-function

--- JS/index.js
+++ JS/index.js
@@ -6,7 +6,7 @@ greeting();
 // Items not needed, in Storage
 let inputItem = document.getElementById("add-item-storage");
 let storageItemsList = document.getElementById("stored-items");
-let itemArrayStorage = [];
+const itemArrayStorage = [];
 
 function addItemStorage() {
   let newItemStorage = inputItem.value;
@@ -14,22 +14,26 @@ function addItemStorage() {
   itemArrayStorage.push(newItemStorage);
 }
 
+// function deleteItemStorage() {
+//   itemArrayStorage.splice(0, 1);
+// }
+
 // Items to purchase
 let inputPurchase = document.getElementById("add-item-purchase");
 let inputPrice = document.getElementById("price-add");
 let purchaseItemsList = document.getElementById("purchases");
-let itemArrayPurchase = [];
+const itemArrayPurchase = [];
 
 function addItemPurchase() {
   let newItemPurchase = inputPurchase.value;
   let newItemPrice = parseInt(inputPrice.value);
   // console.log(newItemPrice);
-   if (newItemPrice === 0) {
-    alert("ERROR! Price cannot be 0!")
-    return
+  if (newItemPrice === 0) {
+    alert("ERROR! Price cannot be 0!");
+    return;
   } else if (isNaN(newItemPrice)) {
-    alert("ERROR! Input has to be a number!")
-    return
+    alert("ERROR! Input has to be a number!");
+    return;
   }
   purchaseItemsList.innerHTML += `<li>${newItemPurchase}, ${newItemPrice} kr</li>`;
   itemArrayPurchase.push({
@@ -38,13 +42,16 @@ function addItemPurchase() {
   });
 }
 
-function deleteItemPurchase () {
-  itemArrayPurchase.splice(0,1) = prompt ("vil du slette dette? skriv Ja");
-}
+// function deleteItemPurchase() {
+//   itemArrayPurchase.splice(0, 1);
+//   prompt("vil du slette dette? skriv Ja");
+//   console.log(itemArrayPurchase);
+// }
+
 // Low in stock
 let inputLowStorage = document.getElementById("add-item-low");
 let lowStorageItemsList = document.getElementById("low-storage");
-let itemArrayLow = [];
+const itemArrayLow = [];
 
 function addItemLowStock() {
   let newItemLow = inputLowStorage.value;
