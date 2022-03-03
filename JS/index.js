8c827e4f32fe8ea4a8f0cbfa1a2a5d16a4d9e45c
Lindgard <a.b.lindgard@gmail.com>
2022-03-03 03:45:03 +0000

    functions adding items. Formated JS

--- JS/index.js
+++ JS/index.js
@@ -7,13 +7,20 @@ greeting();
 let inputItem = document.getElementById("add-item-storage");
 let storageItemsList = document.getElementById("stored-items");
 function addItemStorage() {
-  let newItem = inputItem.value;
-  storageItemsList.innerHTML += `<li>${newItem}</li>`;
+  let newItemStorage = inputItem.value;
+  storageItemsList.innerHTML += `<li>${newItemStorage}</li>`;
 }
 // Items to purchase
 let inputPurchase = document.getElementById("add-item-purchase");
 let purchaseItemsList = document.getElementById("purchases");
-
+function addItemPurchase() {
+  let newItemPurchase = inputPurchase.value;
+  purchaseItemsList.innerHTML += `<li>${newItemPurchase}</li>`;
+}
 // Low in stock
 let inputLowStorage = document.getElementById("add-item-low");
 let lowStorageItemsList = document.getElementById("low-storage");
+function addItemLowStock() {
+  let newItemLow = inputLowStorage.value;
+  lowStorageItemsList.innerHTML += `<li>${newItemLow}</li>`;
+}
